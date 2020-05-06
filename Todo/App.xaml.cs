4b92511c20a02886e968516ef9dc6f5377c73286
michaelmunkwitzbennett <michaelmunkwitzbennett@gmail.com>
2020-05-06 13:01:26 -0500

    Initial Commit

---
+++ Todo/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Todo
+{
+    public partial class App : Application
+    {
+        static JokesItemDatabase database;
+
+        public App()
+        {
+            InitializeComponent();
+
+            var nav = new NavigationPage(new TodoListPage());
+            nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+            nav.BarTextColor = Color.White;
+
+            MainPage = nav;
+        }
+
+        public static JokesItemDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new JokesItemDatabase();
+                }
+                return database;
+            }
+        }
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+}
+
---
+++ Todo/Views/TodoItemPageCS.cs
@@ -0,0 +1,56 @@
+﻿using Xamarin.Forms;
+
+namespace Todo
+{
+    public class TodoItemPageCS : ContentPage
+    {
+        public TodoItemPageCS()
+        {
+            Title = "Todo Item";
+
+            var nameEntry = new Entry();
+            nameEntry.SetBinding(Entry.TextProperty, "Joke");
+
+
+            var doneSwitch = new Switch();
+            doneSwitch.SetBinding(Switch.IsToggledProperty, "Done");
+
+            var saveButton = new Button { Text = "Save" };
+            saveButton.Clicked += async (sender, e) =>
+            {
+                var todoItem = (TodoItem)BindingContext;
+                await App.Database.SaveItemAsync(todoItem);
+                await Navigation.PopAsync();
+            };
+            
+            var deleteButton = new Button { Text = "Delete" };
+            deleteButton.Clicked += async (sender, e) =>
+            {
+                var todoItem = (TodoItem)BindingContext;
+                await App.Database.DeleteItemAsync(todoItem);
+                await Navigation.PopAsync();
+            };
+
+            var cancelButton = new Button { Text = "Cancel" };
+            cancelButton.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopAsync();
+            };
+            
+            Content = new StackLayout
+            {
+                Margin = new Thickness(20),
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Children =
+                {
+                    new Label { Text = "Joke" },
+                    nameEntry,
+
+                    saveButton,
+                    deleteButton,
+                    cancelButton
+                }
+            };
+        }
+    }
+}
